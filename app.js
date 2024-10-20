049cc783e94c54cdaccce7490026c5eb1f2456ef
vlimbo <vwlimbo@proton.me>
2024-10-20 13:52:05 +0200

    Assign all elements to variables in JS
    All functional elements are assigned to variables in JS

---
+++ app.js
@@ -0,0 +1,6 @@
+let playerScore = document.querySelector(".player-score");
+let totalScore = document.querySelector(".total-score");
+let question = document.querySelector(".main__question");
+const trueBtn = document.querySelector(".main__btn-true");
+const falseBtn = document.querySelector(".main__btn-false");
+const nextBtn = document.querySelector(".main__btn-next");
