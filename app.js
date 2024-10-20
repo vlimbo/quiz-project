4d3b759f45dc4630a5e9f0db5b5f80d5656647aa
vlimbo <vwlimbo@proton.me>
2024-10-20 14:38:46 +0200

    Create function for updating scores
    Crated own function to update score, that gets called once the user clicks an answer.

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
+// objects
+const questions = {
+  question1: "true",
+};
+
+const funfacts = {
+  question1:
+    "Brendan Eich created JS at Netscape in 1995. The initial version of the language was written in just 10 days.",
+};
+
+// variables
 let playerScore = document.querySelector(".player-score");
 let totalScore = document.querySelector(".total-score");
-let question = document.querySelector(".main__question");
+let currentQuestion = document.querySelector(".main__question").textContent;
 const trueBtn = document.querySelector(".main__btn-true");
 const falseBtn = document.querySelector(".main__btn-false");
+let funFactBox = document.querySelector(".main__fun-fact");
 const nextBtn = document.querySelector(".main__btn-next");
+
+// functions
+const checkAnswer = (answer) => {
+  if (answer.textContent === questions.question1) {
+    trueBtn.style.background = "lightgreen";
+    updateScore("correct");
+  } else {
+    answer.style.background = "lightpink";
+    updateScore();
+  }
+};
+
+const displayFact = () => {};
+
+const updateScore = (answer = "no") => {
+  if (answer === "correct") {
+    playerScore.textContent = +playerScore.textContent + 1;
+    totalScore.textContent = +totalScore.textContent + 1;
+  } else {
+    totalScore.textContent = +totalScore.textContent + 1;
+  }
+};
+
+// event listeners
+trueBtn.addEventListener("click", () => checkAnswer(trueBtn)); // Look further into
+falseBtn.addEventListener("click", () => checkAnswer(falseBtn)); // Look further
