04322569f3a63e6f962199526106e64ee51dd01c
vlimbo <vwlimbo@proton.me>
2024-10-21 08:00:00 +0200

    Declare statement variable, questions and alternative buttons
    Declared the variables for questions and statements, as well as targeting buttons inside JS.

---
+++ app.js
@@ -0,0 +1,11 @@
+// variables
+statement = document.querySelector(".main__fun-fact");
+trueBtn = document.querySelector(".main__btn-true");
+falseBtn = document.querySelector(".main__btn-false");
+
+// objects
+const facts = {
+  StringsAreMutuable: false,
+};
+
+console.log(facts.StringsAreMutuable);
